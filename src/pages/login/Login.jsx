b086adbf0bed42a8ef92272ff71343ba34eccb7c
khan <omeryagci3295@gmail.com>
2022-09-11 22:43:44 +0300

    login page styles finished

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,83 @@
 import React from "react";
+import registerImage from "../../assests/registerImage.jpg";
+import bookIcon from "../../assests/booksIconpng.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="vh-100">
+      <div className="container-fluid">
+        <div className="row">
+          <div
+            className="col-sm-6 text-black text-center"
+            style={{ backgroundColor: "#f3f3f3" }}
+          >
+            <div className="px-5 ms-xl-4">
+              <span className="h1 fw-bold mb-0">
+                <img style={{ width: "10rem" }} src={bookIcon} alt="ıcon" />
+              </span>
+            </div>
+            <div className="d-flex justify-content-center align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+              <form style={{ width: "23rem" }}>
+                <h3
+                  className="fw-normal mb-3 pb-3"
+                  style={{ letterSpacing: 1 }}
+                >
+                  Log in
+                </h3>
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email..."
+                  />
+                </div>
+                <div className="form-outline mb-4">
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password..."
+                  />
+                </div>
+                <div className="pt-1 mb-4">
+                  <button
+                    className="btn btn-info btn-lg btn-block"
+                    type="button"
+                  >
+                    Login
+                  </button>
+                </div>
+                <p className="small mb-2 pb-lg-2">
+                  <a className="text-muted" href="#!">
+                    Forgot password?
+                  </a>
+                </p>
+                <p>
+                  Don't have an account?
+                  <Link to="/register" className="link-info">
+                    Register here
+                  </Link>
+                </p>
+                <button>Continie with Google</button>
+              </form>
+            </div>
+          </div>
+          <div className="col-sm-6 px-0 d-none d-sm-block">
+            <img
+              src={registerImage}
+              alt="Login"
+              className="w-100 vh-100"
+              style={{ objectFit: "fill" }}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
