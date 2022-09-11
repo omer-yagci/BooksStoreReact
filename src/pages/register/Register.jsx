c8f15bef1e79bc227fc240139398520933288ae7
khan <omeryagci3295@gmail.com>
2022-09-11 11:31:33 +0300

    register pages styles added

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,69 +1,72 @@
 import React from "react";
-import registerImage from "../../assests/registerImage.jpg";
+import registerImage from "../../assests/register.jpg";
+import registerStyles from "../register/registerstyles.module.scss";
 
 const Register = () => {
   return (
-    <div className="register d-flex justify-content-center">
-      <div className="form-image d-md-block ">
-        <img src={registerImage} alt="sample-movie" />
-      </div>
-      <div className="register-form">
-        <h1 className="form-title display-3 ">Register</h1>
-        <form id="register">
-          <div className="mb-3">
-            <label htmlFor="firstName" className="form-label">
-              First Name
-            </label>
+    <div className="d-flex justify-content-center">
+      <div className="parent">
+        <div className="form-image">
+          <img className="register-image" src={registerImage} alt="register" />
+        </div>
+        <div className="register-form">
+          <h1 className="form-title display-3 ">Register</h1>
+          <form id="register">
+            <div className="mb-3">
+              <label htmlFor="firstName" className="form-label">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="firstName"
+                placeholder="Enter your first name.."
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="lastName" className="form-label">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="lastName"
+                placeholder="Enter your last name.."
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter your email adress.."
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter your password.."
+                required
+              />
+            </div>
             <input
-              type="text"
-              className="form-control"
-              id="firstName"
-              placeholder="Enter your first name.."
-              required
+              type="submit"
+              className="btn btn-primary form-control"
+              value="Register"
             />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="lastName" className="form-label">
-              Last Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="lastName"
-              placeholder="Enter your last name.."
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Enter your email adress.."
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter your password.."
-              required
-            />
-          </div>
-          <input
-            type="submit"
-            className="btn btn-primary form-control"
-            value="Register"
-          />
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
