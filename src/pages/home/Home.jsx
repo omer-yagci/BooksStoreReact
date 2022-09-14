94c8f37cc43d5634d1031c9226f03ce09b4b61f8
khan <omeryagci3295@gmail.com>
2022-09-14 16:39:42 +0300

    homepage  started

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import homeStyles from "../home/home.module.scss";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <form className={homeStyles["form-container"]}>
+        <input type="search" className={homeStyles.input} />
+        <button className={homeStyles.btn}>Search</button>
+      </form>
+    </>
+  );
 };
 
 export default Home;
