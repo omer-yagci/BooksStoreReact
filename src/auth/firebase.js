1faeff6bd5142ef85e11defd5c22a4584acddf86
khan <omeryagci3295@gmail.com>
2022-09-14 11:44:10 +0300

    .env added

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -5,7 +5,14 @@ import { getAuth } from "firebase/auth";
 //* https://firebase.google.com/docs/auth/web/start
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
