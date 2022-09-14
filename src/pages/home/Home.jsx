44ae14c1e6f54fc1a8258ad0cfe38da94980f5c4
khan <omeryagci3295@gmail.com>
2022-09-14 19:12:39 +0300

    request to API

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import homeStyles from "../home/home.module.scss";
+import axios from "axios";
 const Home = () => {
+  const [value, setValue] = useState("");
+  const [booksDatas, setBooksDatas] = useState([]);
+
+  const inputChangeHandler = (event) => {
+    setValue(event.target.value);
+    console.log("zaaaa");
+  };
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    getDataFromAPI();
+    setValue(event.target.value);
+  };
+
+  const getDataFromAPI = async () => {
+    try {
+      const options = {
+        method: "GET",
+        url: `https://hapi-books.p.rapidapi.com/search/${value}`,
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_HAPI_KEY,
+          "X-RapidAPI-Host": process.env.REACT_APP_API_HOST,
+        },
+      };
+
+      await axios.request(options).then(function (response) {
+        setBooksDatas(response.data);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(booksDatas);
   return (
     <>
-      <form className={homeStyles["form-container"]}>
-        <input type="search" className={homeStyles.input} />
+      <form
+        onSubmit={formSubmitHandler}
+        className={homeStyles["form-container"]}
+      >
+        <input
+          onChange={inputChangeHandler}
+          type="search"
+          className={homeStyles.input}
+        />
         <button className={homeStyles.btn}>Search</button>
       </form>
     </>
