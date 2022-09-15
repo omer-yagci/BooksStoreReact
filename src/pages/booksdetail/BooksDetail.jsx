a774261d4ce6ee119445d53ca3a45aaacc615d33
khan <omeryagci3295@gmail.com>
2022-09-15 17:49:00 +0300

    detail page added

---
+++ src/pages/booksdetail/BooksDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BooksDetail = () => {
+  return <div>BooksDetail</div>;
+};
+
+export default BooksDetail;
--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "../components/nav/Navbar";
+import BooksDetail from "../pages/booksdetail/BooksDetail";
 import Contact from "../pages/contact/Contact";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
@@ -11,11 +12,19 @@ const Approuter = () => {
     <BrowserRouter>
       <Navbar />
       <Routes>
+        {/* Private Router */}
         <Route path="/" element={<PrivateRouter />}>
           <Route path="" element={<Home />} />
         </Route>
+
         <Route path="register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+
+        {/* Private Router */}
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<BooksDetail />} />
+        </Route>
+
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
