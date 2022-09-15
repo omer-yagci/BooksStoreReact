9e9f691f4524c59ab5cf7cac69eec54bb841095f
khan <omeryagci3295@gmail.com>
2022-09-15 18:33:48 +0300

    contact page done!

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import contactStyles from "../contact/contact.module.scss";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={contactStyles.container}>
+      <section className={contactStyles.section}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/omer-yagci"
+        >
+          <AiFillGithub
+            className={`${contactStyles.icon} ${contactStyles.black}`}
+          />
+        </a>
+      </section>
+      <section className={contactStyles.section}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/omer-kagan-yagci/"
+        >
+          <AiFillLinkedin className={contactStyles.icon} />
+        </a>
+      </section>
+    </div>
+  );
 };
 
 export default Contact;
