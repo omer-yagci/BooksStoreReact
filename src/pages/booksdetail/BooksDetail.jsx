dcd980eb490723f34558d6fc9b60cdadd31a77c5
khan <omeryagci3295@gmail.com>
2022-09-19 13:25:23 +0300

    details page start

--- src/pages/booksdetail/BooksDetail.jsx
+++ src/pages/booksdetail/BooksDetail.jsx
@@ -7,9 +7,26 @@ const BooksDetail = () => {
   // Get data with using useLocation func.
 
   const booksData = location.state;
-  // console.log(booksData);
-  // const { id } = booksData;
-  // console.log(id);
+  console.log(booksData);
+  const {
+    id,
+    accessInfo: {
+      country,
+      epub: { isAvailable },
+      pdf: { isAvailable: pdfAvailable },
+    },
+    saleInfo: { isEbook, saleability },
+    volumeInfo: {
+      authors,
+      categories,
+      description,
+      language,
+      title,
+      previewLink: onlineLink,
+      imageLinks: { thumbnail },
+    },
+  } = booksData;
+  console.log(onlineLink);
   return <div>BooksDetail</div>;
 };
 
